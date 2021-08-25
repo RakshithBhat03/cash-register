880af2018de5a60ecea5fc3691bddfdcef2a8fc6
RakshithBhat03 <rakshith6666@gmail.com>
2021-08-25 14:47:54 +0530

    fix: updated calculateReturn function

--- src/App.js
+++ src/App.js
@@ -18,8 +18,7 @@ function App() {
             placeholder="$"
             className="enter-amount"
             onChange={cashInputHandler}
-            type="number"
-          ></input>
+            type="number"></input>
         </div>
       );
       setNextbutton(nextButton);
@@ -34,64 +33,82 @@ function App() {
     }
   }
   function calculateReturn(inputCash, billAmount) {
-    var returnChange = Number(inputCash) - billAmount;
-    var listDenomination = [2000, 500, 100, 20, 10, 5, 1];
-    var listNumuberOfNotes = [];
-    for (let i = 0; i < 7; i++) {
-      listNumuberOfNotes.push(Math.trunc(returnChange / listDenomination[i]));
-      returnChange = returnChange % listDenomination[i];
+    if (inputCash < billAmount) {
+      var difference = Number(billAmount) - Number(inputCash);
+      checkButton = (
+        <span
+          style={{
+            color: "var(--primary-color)",
+            fontWeight: "500",
+            fontSize: "1.5rem",
+          }}>
+          You're short by{" "}
+          <span style={{ fontWeight: "700", color: "#F87171" }}>
+            {difference}!
+          </span>
+        </span>
+      );
+      setCheckbutton(checkButton);
+      setNotesTable("");
+    } else {
+      var returnChange = Number(inputCash) - billAmount;
+      var listDenomination = [2000, 500, 100, 20, 10, 5, 1];
+      var listNumuberOfNotes = [];
+      for (let i = 0; i < 7; i++) {
+        listNumuberOfNotes.push(Math.trunc(returnChange / listDenomination[i]));
+        returnChange = returnChange % listDenomination[i];
+      }
+      notesTable = (
+        <div className="container">
+          <table className="notes-table">
+            <caption
+              style={{
+                margin: "1rem",
+                color: "var(--primary-color)",
+                fontWeight: "700",
+                fontSize: "1.5rem",
+              }}>
+              Return Change 💲
+            </caption>
+            <tr>
+              <th>Number of notes</th>
+              <td className="notes-returned">
+                {checkNotes(listNumuberOfNotes[0])}
+              </td>
+              <td className="notes-returned">
+                {checkNotes(listNumuberOfNotes[1])}
+              </td>
+              <td className="notes-returned">
+                {checkNotes(listNumuberOfNotes[2])}
+              </td>
+              <td className="notes-returned">
+                {checkNotes(listNumuberOfNotes[3])}
+              </td>
+              <td className="notes-returned">
+                {checkNotes(listNumuberOfNotes[4])}
+              </td>
+              <td className="notes-returned">
+                {checkNotes(listNumuberOfNotes[5])}
+              </td>
+              <td className="notes-returned">
+                {checkNotes(listNumuberOfNotes[6])}
+              </td>
+            </tr>
+            <tr>
+              <th>Denomination</th>
+              <td>2000</td>
+              <td>500</td>
+              <td>100</td>
+              <td>20</td>
+              <td>10</td>
+              <td>5</td>
+              <td>1</td>
+            </tr>
+          </table>
+        </div>
+      );
+      setNotesTable(notesTable);
     }
-    notesTable = (
-      <div className="container">
-        <table className="notes-table">
-          <caption
-            style={{
-              margin: "1rem",
-              color: "var(--primary-color)",
-              fontWeight: "700",
-              fontSize: "1.5rem",
-            }}
-          >
-            Return Change 💲
-          </caption>
-          <tr>
-            <th>Number of notes</th>
-            <td className="notes-returned">
-              {checkNotes(listNumuberOfNotes[0])}
-            </td>
-            <td className="notes-returned">
-              {checkNotes(listNumuberOfNotes[1])}
-            </td>
-            <td className="notes-returned">
-              {checkNotes(listNumuberOfNotes[2])}
-            </td>
-            <td className="notes-returned">
-              {checkNotes(listNumuberOfNotes[3])}
-            </td>
-            <td className="notes-returned">
-              {checkNotes(listNumuberOfNotes[4])}
-            </td>
-            <td className="notes-returned">
-              {checkNotes(listNumuberOfNotes[5])}
-            </td>
-            <td className="notes-returned">
-              {checkNotes(listNumuberOfNotes[6])}
-            </td>
-          </tr>
-          <tr>
-            <th>Denomination</th>
-            <td>2000</td>
-            <td>500</td>
-            <td>100</td>
-            <td>20</td>
-            <td>10</td>
-            <td>5</td>
-            <td>1</td>
-          </tr>
-        </table>
-      </div>
-    );
-    setNotesTable(notesTable);
   }
   function cashInputHandler(event) {
     var inputCash = event.target.value;
@@ -101,8 +118,7 @@ function App() {
         <div className="container">
           <button
             id="check-button"
-            onClick={() => calculateReturn(inputCash, billAmount)}
-          >
+            onClick={() => calculateReturn(inputCash, billAmount)}>
             Check
           </button>
         </div>
@@ -120,8 +136,7 @@ function App() {
             color: "var(--primary-color)",
             fontWeight: "500",
             fontSize: "1.5rem",
-          }}
-        >
+          }}>
           You're short by{" "}
           <span style={{ fontWeight: "700", color: "#F87171" }}>
             {difference}!
@@ -151,8 +166,7 @@ function App() {
             fontWeight: "500",
             fontSize: "1.2rem",
             marginBottom: "3.5rem",
-          }}
-        >
+          }}>
           Enter the bill amount and cash given and I'll let you know the minimum
           number of notes to be returned
         </h2>
@@ -162,8 +176,7 @@ function App() {
           className="enter-amount"
           onChange={billInputHandler}
           type="number"
-          placeholder="$"
-        ></input>
+          placeholder="$"></input>
         {nextButton}
         {checkButton}
         {notesTable}
